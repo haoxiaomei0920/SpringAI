8eaa0249fb00737078d53af33f0e40561dcd5fa7
sodamay <yawen52014@126.com>
2025-06-04 00:07:36 +0800

    bug: qulifiter

--- src/main/java/org/example/ai/config/ChatClientConfig.java
+++ src/main/java/org/example/ai/config/ChatClientConfig.java
@@ -6,9 +6,20 @@ import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ai.chat.model.ChatModel;
 
-//@Configuration
-//public class ChatClientConfig {
+@Configuration
+public class ChatClientConfig {
+    private final ChatModel deepSeekChatModel;
+
+    public ChatClientConfig(ChatModel deepSeekChatModel) {
+        this.deepSeekChatModel = deepSeekChatModel;
+    }
+
+    @Bean
+    ChatClient.Builder chatClientBuilder() {
+        return ChatClient.builder(this.deepSeekChatModel);
+    }
 //    @Bean
 //    ChatClient chatClient(ChatClient.Builder builder) {
 //        ChatClient chatClient = builder.build();
@@ -16,17 +27,16 @@ import org.springframework.context.annotation.Configuration;
 //        //System.out.println(res);
 //        return chatClient;
 //    }
-//    @Bean
-//    @Qualifier("openAiChatClient")
-//    public ChatClient openAiChatClient(OpenAiChatModel chatModel) {
-//        return ChatClient.create(chatModel);
-//    }
-//
-//
-//    @Bean
-//    @Qualifier("deepSeekChatClient")
-//    public ChatClient deepSeekChatClient(DeepSeekChatModel  chatModel) {
-//        return ChatClient.create(chatModel);
-//    }
-//
-//}
+    @Bean
+    @Qualifier("openAiChatClient")
+    public ChatClient openAiChatClient(OpenAiChatModel chatModel) {
+        return ChatClient.create(chatModel);
+    }
+
+
+    @Bean
+    @Qualifier("deepSeekChatClient")
+    public ChatClient deepSeekChatClient(DeepSeekChatModel  chatModel) {
+        return ChatClient.create(chatModel);
+    }
+}
