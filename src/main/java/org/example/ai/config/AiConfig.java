dca2b70470c2d3ea1fe5840e7ed4c1888714740d
sodamay <1598056487@qq.com>
2025-06-11 23:21:42 +0800

    feat: opt

--- src/main/java/org/example/ai/config/AiConfig.java
+++ src/main/java/org/example/ai/config/AiConfig.java
@@ -7,29 +7,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-//@Configuration
-//public class AiConfig {
-//    private final ChatModel deepSeekChatModel;
-//
-//    public AiConfig(DeepSeekChatModel deepSeekChatModel) {
-//        this.deepSeekChatModel = deepSeekChatModel;
-////        String res = this.deepSeekChatModel.call("请问你是deepseek吗？");
-////        System.out.println(res);
-//    }
-//
-//    @Bean
-//    ChatClient.Builder chatClientBuilder() {
-//        return ChatClient.builder(this.deepSeekChatModel);
-//    }
-//    @Bean
-//    ChatClient chatClient(ChatClient.Builder builder) {
-//        ChatClient chatClient = builder.build();
-////        String res = chatClient.prompt("Tell me a joke").call().content();
-//        //System.out.println(res);
-//        return chatClient;
-//    }
-//
-//    public ChatModel getDeepSeekChatModel() {
-//        return deepSeekChatModel;
-//    }
-//}
+@Configuration
+public class AiConfig {
+    private final ChatModel deepSeekChatModel;
+
+    public AiConfig(DeepSeekChatModel deepSeekChatModel) {
+        this.deepSeekChatModel = deepSeekChatModel;
+//        String res = this.deepSeekChatModel.call("请问你是deepseek吗？");
+//        System.out.println(res);
+    }
+
+    @Bean
+    ChatClient.Builder chatClientBuilder() {
+        return ChatClient.builder(this.deepSeekChatModel);
+    }
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder) {
+        ChatClient chatClient = builder.build();
+//        String res = chatClient.prompt("Tell me a joke").call().content();
+        //System.out.println(res);
+        return chatClient;
+    }
+
+    public ChatModel getDeepSeekChatModel() {
+        return deepSeekChatModel;
+    }
+}
